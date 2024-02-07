b132f7bb7ffa5d932b61eb757d078b22bf28f4ba
Lior Bas <look551@gmail.com>
2024-02-07 15:58:09 +0200

    I added searchBinary

--- script/script.js
+++ script/script.js
@@ -3,19 +3,39 @@
 //     $('[type="password"]').val('nir12345');
 // });
 
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5];
 
-let searchMissingNumber = function(array) {
-    const number = array.length + 1;
-    const sum = (number * (number + 1)) / 2;
+// let searchMissingNumber = function(array) {
+//     const number = array.length + 1;
+//     const sum = (number * (number + 1)) / 2;
 
-    let newSum = 0;
-    array.forEach(number => {
-        newSum += number;
-    });
+//     let newSum = 0;
+//     array.forEach(number => {
+//         newSum += number;
+//     });
 
-    return sum - newSum;
+//     return sum - newSum;
+// }
+
+// console.log(searchMissingNumber(array));
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let binarySearch = function(array, number, start = 0, end = array.length - 1) {
+    if(start > end) {
+        return false;
+    }
+    
+    const middle = Math.floor((start + end) / 2);
+
+    if(array[middle] === number) {
+        return middle;
+    } else if(array[middle] > number) {
+        return binarySearch(array, number, start, middle - 1);
+    } else {
+        return binarySearch(array, number, middle + 1, end);
+    }
+
 }
 
-console.log(searchMissingNumber(array));
-
+console.log(binarySearch(array, 6))
